e8f450ce3ab8aba15cb752ff70473bb77dfb6e6e
christngono <larrabyghost@gmail.com>
2026-04-29 04:55:16 +0100

    caht area

---
+++ src/components/MarkdownRenderer.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+export default function MarkdownRenderer({ children }: { children: string }) {
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
+  );
+}
